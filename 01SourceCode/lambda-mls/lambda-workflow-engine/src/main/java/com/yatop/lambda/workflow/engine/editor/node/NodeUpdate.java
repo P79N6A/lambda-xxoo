07b99c378b706c44aa00f9f3b3b0a0b43a22ccc9
lidd <tomlee714@126.com>
2018-12-30 01:46:33 +0800

    update

--- 01SourceCode/lambda-mls/lambda-workflow-engine/src/main/java/com/yatop/lambda/workflow/engine/editor/node/NodeUpdate.java
+++ 01SourceCode/lambda-mls/lambda-workflow-engine/src/main/java/com/yatop/lambda/workflow/engine/editor/node/NodeUpdate.java
@@ -1,8 +1,8 @@
 package com.yatop.lambda.workflow.engine.editor.node;
 
+import com.yatop.lambda.base.model.WfFlowNode;
 import com.yatop.lambda.core.mgr.workflow.node.NodeMgr;
 import com.yatop.lambda.workflow.core.context.WorkflowContext;
-import com.yatop.lambda.workflow.core.richmodel.workflow.node.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +13,8 @@ public class NodeUpdate {
     NodeMgr nodeMgr;
 
     //名称、X坐标、Y坐标、警告消息、节点状态、注释、概要、描述
-    public void updateNode(WorkflowContext workflowContext, Node node) {
+    public WfFlowNode updateNode(WorkflowContext workflowContext, WfFlowNode node) {
         nodeMgr.updateNode(node, workflowContext.getOperId());
-        node.copyProperties(nodeMgr.queryNode(node.getNodeId()));
+        return nodeMgr.queryNode(node.getNodeId());
     }
 }
