ce7715cc72f168c2a329eae97a6e79f0c9a15330
lidd <tomlee714@126.com>
2019-02-01 12:34:30 +0800

    update

---
+++ 01SourceCode/lambda-mls/lambda-framework/src/main/java/com/yatop/lambda/framework/config/AsyncExecutorConfig.java
@@ -0,0 +1,28 @@
+package com.yatop.lambda.framework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncExecutorConfig {
+
+    @Bean
+    public Executor getExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int corePoolSize = 4;
+        int maxPoolSize = 16;
+        int queueCapacity = 64;
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("Async-Executor-");
+        executor.initialize();
+        return executor;
+    }
+}
